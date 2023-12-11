0c3bebcf1f74e7278db889da00eef401bd4be84b
andero2003 <andero2003@gmail.com>
2023-12-11 22:02:33 +0000

    Cool stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -20,6 +21,23 @@ class _MainAppState extends State<MainApp> {
 
   PageController _pageController = PageController();
 
+  List<LinearGradient> _backgroundGradients = [
+    LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [Color.fromARGB(255, 11, 26, 73), const Color.fromARGB(255, 20, 82, 133)],
+    ),
+    LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [Color.fromARGB(255, 14, 78, 94), Color.fromARGB(255, 5, 94, 79)],
+    ),
+    LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [Colors.blueGrey.shade800, Colors.blueGrey.shade400],
+    ),
+  ];
   var views = <Widget>[HomeScreen(), AboutScreen(), ProjectsScreen()];
 
   @override
@@ -31,80 +49,79 @@ class _MainAppState extends State<MainApp> {
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              stops: [0.0, 1],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(255, 19, 19, 19),
-                Color.fromARGB(255, 14, 3, 53),
-              ],
-              tileMode: TileMode.repeated,
+        body: Stack(
+          children: [
+            AnimatedContainer(
+              duration: Duration(milliseconds: 250),
+              decoration: BoxDecoration(
+                gradient: _backgroundGradients[currentPageIndex],
+              ),
             ),
-          ),
-          child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Stack(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: const Color.fromARGB(255, 146, 146, 146), width: 2),
-                        borderRadius: BorderRadius.circular(16)),
-                  ),
-                  PageView(
-                      controller: _pageController,
-                      onPageChanged: (index) {
-                        setState(() {
-                          currentPageIndex = index;
-                        });
-                      },
-                      children: views),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: [
-                        TextButton(
-                            onPressed: () {
-                              _pageController.animateToPage(
-                                0,
-                                duration: Duration(milliseconds: 500),
-                                curve: Curves.easeInOut,
-                              );
-                            },
-                            child: Text("Home",
-                                style: TextStyle(
-                                    color: currentPageIndex == 0 ? const Color.fromARGB(255, 59, 104, 228) : Colors.white, fontSize: 24))),
-                        SizedBox(width: 16),
-                        TextButton(
-                            onPressed: () {
-                              _pageController.animateToPage(
-                                1,
-                                duration: Duration(milliseconds: 500),
-                                curve: Curves.easeInOut,
-                              );
-                            },
-                            child: Text("About",
-                                style: TextStyle(
-                                    color: currentPageIndex == 1 ? const Color.fromARGB(255, 59, 104, 228) : Colors.white, fontSize: 24))),
-                        SizedBox(width: 16),
-                        TextButton(
-                            onPressed: () {
-                              _pageController.animateToPage(
-                                2,
-                                duration: Duration(milliseconds: 500),
-                                curve: Curves.easeInOut,
-                              );
-                            },
-                            child: Text("Projects",
-                                style: TextStyle(
-                                    color: currentPageIndex == 2 ? const Color.fromARGB(255, 59, 104, 228) : Colors.white, fontSize: 24))),
-                      ],
+            Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: const Color.fromARGB(255, 146, 146, 146), width: 2),
+                          borderRadius: BorderRadius.circular(16)),
                     ),
-                  ),
-                ],
-              )),
+                    PageView(
+                        controller: _pageController,
+                        onPageChanged: (index) {
+                          setState(() {
+                            currentPageIndex = index;
+                          });
+                        },
+                        children: views),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        children: [
+                          TextButton(
+                              onPressed: () {
+                                _pageController.animateToPage(
+                                  0,
+                                  duration: Duration(milliseconds: 500),
+                                  curve: Curves.easeInOut,
+                                );
+                              },
+                              child: Text("Home",
+                                  style: TextStyle(
+                                      color: currentPageIndex == 0 ? const Color.fromARGB(255, 59, 104, 228) : Colors.white,
+                                      fontSize: 24))),
+                          SizedBox(width: 16),
+                          TextButton(
+                              onPressed: () {
+                                _pageController.animateToPage(
+                                  1,
+                                  duration: Duration(milliseconds: 500),
+                                  curve: Curves.easeInOut,
+                                );
+                              },
+                              child: Text("About",
+                                  style: TextStyle(
+                                      color: currentPageIndex == 1 ? const Color.fromARGB(255, 59, 104, 228) : Colors.white,
+                                      fontSize: 24))),
+                          SizedBox(width: 16),
+                          TextButton(
+                              onPressed: () {
+                                _pageController.animateToPage(
+                                  2,
+                                  duration: Duration(milliseconds: 500),
+                                  curve: Curves.easeInOut,
+                                );
+                              },
+                              child: Text("Projects",
+                                  style: TextStyle(
+                                      color: currentPageIndex == 2 ? const Color.fromARGB(255, 59, 104, 228) : Colors.white,
+                                      fontSize: 24))),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+          ],
         ),
       ),
     );
@@ -312,6 +329,73 @@ class ProjectsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("TBD"));
+    return Center(
+        child: CarouselSlider(
+      options: CarouselOptions(
+          height: MediaQuery.of(context).size.height * 0.6, enlargeCenterPage: true, clipBehavior: Clip.none, viewportFraction: 0.5),
+      items: [1, 2, 3, 4, 5].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return AspectRatio(
+              aspectRatio: 0.85,
+              child: Card(
+                elevation: 10,
+                color: Color.fromARGB(255, 9, 6, 29),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16)),
+                          child: Image.network(
+                            'https://cdn.discordapp.com/attachments/990339971040821258/1130589877600276540/Ball_Earth_Thumb.jpg?ex=6589df60&is=65776a60&hm=0f63336d7cf73f857bad504533a7302ed628c51678b944e16bcf3bc58c00d5ff&',
+                            width: double.infinity,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 10,
+                          right: 10,
+                          child: FloatingActionButton(
+                            onPressed: () async {
+                              final Uri params = Uri(
+                                scheme: 'https',
+                                host: 'www.roblox.com',
+                                path: '/games/13814171092/Balloon-Simulator',
+                              );
+
+                              if (await canLaunchUrl(params)) {
+                                await launchUrl(params);
+                              }
+                            },
+                            backgroundColor: Colors.white,
+                            child: Icon(Icons.play_arrow, color: Colors.black),
+                          ),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 4),
+                      child: Text(
+                        'Balloon Simulator',
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                      child: Text(
+                        'Balloon Simulator is a Roblox game that has garnered over 200M plays. It is a game where you collect balloons and upgrade your backpack to become the best balloon collector in the world.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      }).toList(),
+    ));
   }
 }
